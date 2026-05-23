472b06471c205b5776f025d75e30e50411f2bdfd
joaorodskt <144625686+joaorodskt@users.noreply.github.com>
2026-05-23 11:25:28 +0200

    Project created

---
+++ src/main/java/demo/MovingHelper/MovingHelperApplication.java
@@ -0,0 +1,13 @@
+package demo.MovingHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovingHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovingHelperApplication.class, args);
+	}
+
+}
