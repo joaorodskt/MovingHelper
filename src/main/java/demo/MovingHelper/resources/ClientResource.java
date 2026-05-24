344be0fb4681b6b530d1463878efb53d55646ca9
joaorodskt <144625686+joaorodskt@users.noreply.github.com>
2026-05-24 11:01:31 +0200

    Service layer, component registration, findById

--- src/main/java/demo/MovingHelper/resources/ClientResource.java
+++ src/main/java/demo/MovingHelper/resources/ClientResource.java
@@ -1,23 +1,33 @@
 package demo.MovingHelper.resources;
 
-import java.time.LocalDate;
-import java.time.Month;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import demo.MovingHelper.domain.Address;
 import demo.MovingHelper.domain.Client;
+import demo.MovingHelper.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
+	
+	@Autowired
+	private ClientService service;
 
 	@GetMapping
-	public ResponseEntity<Client> findAll(){
-		Client c = new Client(1L, "Carles", "Lopez", "Z2334567A", "clopz@gmail.com", "345234786", LocalDate.of(2001, Month.JANUARY, 13), new Address(1L, "Carrer Pau Claris", 14, "Barcelona", "Catalunya", "España", "08014"));
-		return ResponseEntity.ok().body(c);
-	}	
+	public ResponseEntity<List<Client>> findAll(){
+		List<Client> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Client> findById(@PathVariable Long id){ //valor do parametro é o passado no endpoint
+		Client obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
